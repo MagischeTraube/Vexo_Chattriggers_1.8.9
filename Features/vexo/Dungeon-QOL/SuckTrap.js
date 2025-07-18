89f3020a24792717da5ae5cc2df9d13786fe4301
lloyd838 <pk.bearlest@gmail.com>
2025-07-18 23:51:12 +0200

    Removed not needed imports

--- Features/vexo/Dungeon-QOL/SuckTrap.js
+++ Features/vexo/Dungeon-QOL/SuckTrap.js
@@ -1,7 +1,3 @@
-import { registerWhen } from "../../../../BloomCore/utils/Utils";
-import { prefix_vexo } from "../../../utils/util"
-
-
 export const SuckTrapCommand = register("command", () => {
     ChatLib.command("p please learn how to trap... tutorial -> youtube.com", false)
 }).setName("SuckTrap");
