89893bb9d036dff5f0068290ace8cf921199af42
lloyd838 <pk.bearlest@gmail.com>
2025-07-18 15:03:17 +0200

    NoMageSheep now only triggers when you are a Mage

--- Features/vexo/Dungeon-QOL/StopMageSheepRendering.js
+++ Features/vexo/Dungeon-QOL/StopMageSheepRendering.js
@@ -1,17 +1,34 @@
 import { registerWhen } from "../../../../BloomCore/utils/Utils"
 import config from "../../../config"
-import { isInDungeon } from "../../../utils/util"
+import { getClass } from "../../../utils/util"
 
-register(`worldLoad`, () => {
-    inDungeon = isInDungeon();
-});
+const S34PacketMaps = Java.type("net.minecraft.network.play.server.S34PacketMaps")
+let isMage = false;
 
 register(`worldUnload`, () => {
     inDungeon = false;
+    isMage = false;
 });
+
+registerWhen(register("chat", (msg) => { 
+    runStartRegister.register()
+
+    return msg;
+}).setCriteria("Starting in 1 second."), () => config.NoMageSheep);
+
+const runStartRegister = register("PacketReceived", () => {
+    runStartRegister.unregister();
+
+    setTimeout(() => {
+        if (getClass() == "Mage") {
+            isMage = true;
+        }
+    }, 1000)
+
+}).setFilteredClass(S34PacketMaps).unregister()
 
 registerWhen(register("renderEntity", (entity, poss, pt, event) => {
     if (entity.getName() === "Sheep") {    
         cancel(event)
     }
-}), () => (config.NoMageSheep && inDungeon))
+}), () => config.NoMageSheep && isMage)
--- Features/vexo/Dungeon-QOL/sbTNT.js
+++ Features/vexo/Dungeon-QOL/sbTNT.js
@@ -6,10 +6,7 @@ registerWhen(register("chat", (msg) => {
     ChatLib.chat(`${prefix_vexo} §aGetting Superboom TNT`);
     if (!config.sbtnt) return msg;
 
-
-    setTimeout(() => {
-        ChatLib.command("od sb", true);
-    }, 100);
+    ChatLib.command("od sb", true);
 
     return msg;
-}).setCriteria("Starting in 4 seconds.").setContains(), () => config.sbtnt);
+}).setCriteria("Starting in 2 seconds.").setContains(), () => config.sbtnt);
