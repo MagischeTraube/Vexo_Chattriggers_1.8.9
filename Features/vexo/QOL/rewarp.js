bc33c270f6487b21f605f6ba33657fdbcc8d56e3
lloyd838 <pk.bearlest@gmail.com>
2025-07-19 16:17:57 +0200

    rewarp with getDistance() function

--- Features/vexo/QOL/rewarp.js
+++ Features/vexo/QOL/rewarp.js
@@ -1,17 +1,22 @@
 import config from "../../../config";
 import { registerWhen } from "../../../../BloomCore/utils/Utils";
-import { playercoords, prefix_vexo } from "../../../utils/util";
+import { getDistance, playercoords, prefix_vexo } from "../../../utils/util";
+import { DevMessage } from "../../../utils/DevMessage";
 
 let rewarpprogress = false;
 
 
-const rewarpfunction = () => {
+function rewarpfunction() {
     let rewarpprogress = true;
     ChatLib.command("is", false);
+    DevMessage("rewarpfunction warped to island")
     setTimeout(() => {
+        DevMessage("rewarpfunction warping!")
         ChatLib.command(config.rewarpto, false);
+        DevMessage("rewarpfunction warped to config.rewarpto")
         let rewarpprogress = false;
-    }, 3500);}
+    }, 5000);
+}
 
 
 export const RewarpCommand = register("command", () => {
@@ -24,15 +29,14 @@ register("chat", (msg) => {
 }).setCriteria("MONOLITH! You found a mysterious Dark Monolith").setContains();
 
 
-
-register("step", () => {
+registerWhen(register("step", () => {
     if (!config.rewarpcoordstoggle || rewarpprogress || !config.rewarp) return;
     let [x, y, z] = playercoords();
-    let gesx = 26.5;
-    let gesy = 180;
-    let gesz = 105.5;
-    if (((gesx - 1) <= x) && (x <= (gesx + 1)) && ((gesy - 1) <= y) && (y <= (gesy + 1)) && (((gesz - 1) <= z) && (z <= (gesz + 1)))) {
+    let [gesx, gesy, gesz] = config.rewarpcoords.split(",")
+    DevMessage(`Rewarp coords: x: ${gesx} | y: ${gesy} | z: ${gesz}`);
+
+    if (getDistance(x, y, z, gesx, gesy, gesz) <= 3) {
         ChatLib.chat(`${prefix_vexo} §aRewarping`);
-        ChatLib.chat(`x: ${x} y: ${y} z: ${z}`);
+        DevMessage(`Close to coords with x: ${x} | y: ${y} | z: ${z}`);
     }
-}).setFps(5);
+}).setFps(5), () => config.rewarpcoords && !rewarpprogress)
--- utils/util.js
+++ utils/util.js
@@ -21,6 +21,10 @@ export function getClass() {
     return match[1];
 }
 
+export function getDistance(x1, y1, z1, x2, y2, z2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+}
+
 // Credits FreshNotifier
 export function getAllPlayers() {
     const players = World
